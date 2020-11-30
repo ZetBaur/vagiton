4f4842487cd7c8549c1b027da5bb05fc2d79a003
Your Name <you@example.com>
2020-11-30 16:35:16 +0600

    main page

--- #source/js/scripts/script.js
+++ #source/js/scripts/script.js
@@ -2,7 +2,19 @@
 
 $(document).ready(function(){
 
+$(window).scroll(function(){
+	if($(this).scrollTop() > 1000){
+		$('.pageup').fadeIn();
+	}else{
+		$('.pageup').fadeOut();
 
+	}
+});
+$("a[href^='#']").click(function(){
+	var _href = $(this).attr("href");
+	$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+	return false;
+});
 
 // =====================SLIDER================================================================================
 
