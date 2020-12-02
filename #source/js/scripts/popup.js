f0e09934e472c00c004ab99d783b42223ab5cde3
Your Name <you@example.com>
2020-12-02 15:42:43 +0600

    finish

--- #source/js/scripts/popup.js
+++ #source/js/scripts/popup.js
@@ -2,11 +2,11 @@
 $(document).ready(function(e){
 	$('.popup-link').on('click', function(){
 		$('.popup__overlay, .popup__window').fadeIn();
+		if ($('.header__menu-list').is('.open')) {
+			$('.header__menu-list, .header__burger ').removeClass('open');
+		}
 	});
 	$('.popup__close').on('click', function(){
 		$('.popup__overlay, .popup__window').fadeOut();
 	});
-
-	e.preventDefault();
-
 });
--- #source/js/scripts/script.js
+++ #source/js/scripts/script.js
@@ -1,6 +1,7 @@
 
 
-$(document).ready(function(){
+$(document).ready(function () {
+
 	// =====================SLIDER================================================================================
 
 	$('.production__item-slider').slick({
@@ -16,110 +17,81 @@ $(document).ready(function(){
 
 	// =====================SCROLL TO TOP================================================================================
 
-$(window).scroll(function(){
-	if($(this).scrollTop() > 900){
-		$('.pageup').fadeIn();
-	}else{
-		$('.pageup').fadeOut();
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 900) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
 
-	}
-});
-$("a[href^='#']").click(function(){
-	var _href = $(this).attr("href");
-	$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-	return false;
-});
-// =====================FORM Validate================================================================================
-$("input[name=phone]").mask("+7 (999) 999-99-99");
+		}
+	});
+	$("a[href^='#']").click(function () {
+		var _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
+	});
+	// =====================FORM Validate================================================================================
 
-	  function validateForms (form){
+	$("input[name=phone]").mask("+7 (999) 999-99-99");
+
+	function validateForms(form) {
 		$(form).validate({
 			rules: {
-			  name: {
-				required: true,
-				minlength: 2
-			  },
-			  phone: "required",
-			  email: {
-				required: true,
-				email: true
-			  }
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
 			},
 			messages: {
-			  name:  {
-				required: "We need your email address to contact you",
-				minlength: jQuery.validator.format("At least {0} characters required!")
-			  },
-			  email: {
-				required: "We need your email address to contact you",
-				email: "Your email address must be in the format of name@domain.com"
-			  },
-			  phone:{
-				required: "Please enter your phone number",
-			  }
+				name: {
+					required: "We need your email address to contact you",
+					minlength: jQuery.validator.format("At least {0} characters required!")
+				},
+				email: {
+					required: "We need your email address to contact you",
+					email: "Your email address must be in the format of name@domain.com"
+				},
+				phone: {
+					required: "Please enter your phone number",
+				}
 
 
 			}
 
-		
-		  });
-	  }
-	  validateForms('#commentForm');
 
+		});
+	}
+	validateForms('#commentForm');
 
-// =====================MENU ITEMS================================================================================
-  
-$('.header__menu-item').on('click', function(){
-	$('.header__menu-item').removeClass('active');
-	$(this).addClass('active');
-});
+	// ================MENU and BURGER=============================================================================
 
-
-// ================MENU and BURGER=============================================================================
-    $('.burger').on('click', function(){
-        $('.menu').toggleClass('open');
-    });
-
-    $('.menu-item').on('click', function(){
-        if($('.menu').hasClass('open')){
-            $('.menu').removeClass('open');
-        }
-    });
-
-    $('html, body').on('click', function(e){
-        if($('.menu').hasClass('open') && !$(e.target).is('.menu, .menu-item, .burger')){
-            $('.menu').removeClass('open');
-        }
+	$('.header__burger').on('click', function () {
+		$('.header__menu-list, .header__burger ').toggleClass('open');
 	});
-// ================MENU and BURGER=============================================================================
-	
 
-
-
-// =====================ibg================================================================================
-function ibg(){
-	$.each($('.ibg'), function(index, val) {
-		if($(this).find('img').length>0){
-			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+	$('.header__menu-item a, .popup-link').on('click', function () {
+		if ($('.header__menu-list').is('.open')) {
+			$('.header__menu-list, .header__burger ').removeClass('open');
 		}
 	});
-}
-ibg();
-// =====================ibg================================================================================
 
-// ==========UP==================================================================================================
+	// ==========UP==================================================================================================
 
-$(window).scroll(function() {
-		var w=$(window).width();
-	if($(window).scrollTop()>50){
-		$('#up').fadeIn(300);
-	}else{
-		$('#up').fadeOut(300);
-	}
-});
-$('#up').click(function(event) {
-	$('body,html').animate({scrollTop:0},300);
-});
+	$(window).scroll(function () {
+		var w = $(window).width();
+		if ($(window).scrollTop() > 50) {
+			$('#up').fadeIn(300);
+		} else {
+			$('#up').fadeOut(300);
+		}
+	});
+	$('#up').click(function (event) {
+		$('body,html').animate({ scrollTop: 0 }, 300);
+	});
 
 });
-// ==========UP==================================================================================================
